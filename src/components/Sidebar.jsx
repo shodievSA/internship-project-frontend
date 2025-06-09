adb20438f46b5e604e39d8d5dfbab43212bde5e3
Doniyor Aloxanov <doniyoraloxanov@gmail.com>
2025-06-09 18:08:17 +0500

    WIP: local changes to Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,12 +7,12 @@ function Sidebar({ sidebarCollapsed, setSidebarCollapsed }) {
         <div>
             {/* dark, semi-transparent background color only for mobile and tablet screen sizes */}
             <div className={`${sidebarCollapsed ? 'bg-transparent pointer-events-none' : "bg-black/30"} 
-            fixed h-full w-full transition-bg-color duration-200 z-10 lg:hidden`}></div> 
+            fixed h-full w-full transition-bg-color duration-200 z-10 lg:hidden`}></div>
 
             <div className={`${sidebarCollapsed ? 'w-0' : 'w-full lg:w-72'} dark:text-white flex fixed 
             lg:static h-full z-20 transition-[width] duration-500`}>
-                <div className={`${sidebarCollapsed ? 'w-0 border-r-0 dark:border-neutral-800 border-neutral-200' 
-                : 'w-72 border-r-[1px] dark:border-neutral-800 border-neutral-200'} dark:bg-neutral-900 bg-neutral-50 
+                <div className={`${sidebarCollapsed ? 'w-0 border-r-0 dark:border-neutral-800 border-neutral-200'
+                    : 'w-72 border-r-[1px] dark:border-neutral-800 border-neutral-200'} dark:bg-neutral-900 bg-neutral-50 
                 transition-[width] duration-500`}>
                     <div className={`${sidebarCollapsed ? 'invisible' : 'visible'} flex flex-col h-full py-4
                     gap-y-8`}>
@@ -73,4 +73,4 @@ function Sidebar({ sidebarCollapsed, setSidebarCollapsed }) {
 
 }
 
-export default Sidebar;
+export default Sidebar; 
