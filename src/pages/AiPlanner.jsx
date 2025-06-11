67e512e57e6d5d5743b6b279d0811dfd64f95609
Doniyor Aloxanov <doniyoraloxanov@gmail.com>
2025-06-12 01:27:41 +0500

    Remove unnecessary whitespace in AiPlanner component

--- src/pages/AiPlanner.jsx
+++ src/pages/AiPlanner.jsx
@@ -2,7 +2,6 @@ import EmptyAiPlanner from "../components/EmptyAiPlanner";
 
 
 function AiPlanner() {
-
     return (
         <div className="h-full px-5 pt-10 lg:px-10 ">
             <EmptyAiPlanner />
