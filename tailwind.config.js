ae4f2740c881e4b7f43aba0d27ca7fd88a7d4ec3
Abbos Shodiev <shodiev.s.a@gmail.com>
2025-06-12 01:36:28 +0500

    configured tailwind scrollbar plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,21 @@
-/** @type {import('tailwindcss').Config} */
 import defaultTheme from 'tailwindcss/defaultTheme';
+import scrollbar from 'tailwind-scrollbar';
 
 export default {
-  darkMode: ['class', '.dark-mode'],
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        'inter': ['Inter', ...defaultTheme.fontFamily.sans]
-      }
-    },
-  },
-  plugins: [],
+	darkMode: ['class', '.dark-mode'],
+	content: [
+		"./index.html",
+		"./src/**/*.{js,jsx}",
+	],
+	theme: {
+		extend: {
+		fontFamily: {
+			'inter': ['Inter', ...defaultTheme.fontFamily.sans]
+		}
+		},
+	},
+	plugins: [
+		scrollbar()
+	],
 }
 
