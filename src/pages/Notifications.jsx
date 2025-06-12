36ad9584e393c3e5f59ba31b3cbac987fc84c1ec
Abbos Shodiev <shodiev.s.a@gmail.com>
2025-06-12 15:52:23 +0500

    import component for empty state

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
+import EmptyNotifications from "../components/EmptyNotifications";
+
 function Notifications() {
 
+    const [notificationCount, setNotificationCount] = useState(() => {
+
+        try {
+
+            const count = JSON.parse(localStorage.getItem('notificationCount'));
+            return (Number.isInteger(count) && Number.isFinite(count)) ? count : 0;
+
+        } catch {
+
+            return 0;
+
+        }
+
+    });
+
     return (
-        <div>
-            <h1>This is notifications page</h1>
+        <div className="h-full px-5 pt-10 lg:px-10">
+            {
+                notificationCount > 0 ? (
+                    <div></div>
+                ) : (
+                    <EmptyNotifications />
+                )
+            }
         </div>
-    )
+    );
 
 }
 
