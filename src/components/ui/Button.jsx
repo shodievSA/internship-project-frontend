671a3dc526c4e4a2cf28a5c7c11633ba85eec2fa
Abbos Shodiev <shodiev.s.a@gmail.com>
2025-06-16 17:11:01 +0500

    add className prop so that button component could accept additional class names for more flexibility

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -3,19 +3,20 @@ function Button({
     children,
     loading = false,
     disabled,
-    onClick
+    onClick,
+    className
 }) {
 
     const variants = {
-        primary: 'dark:bg-white dark:hover:bg-slate-200 dark:text-black bg-neutral-900 hover:bg-neutral-900/90 text-white py-2.5 px-4 rounded-lg font-medium text-sm lg:text-base flex justify-center disabled:opacity-50',
-        secondary: 'dark:bg-neutral-950 dark:border-neutral-800 dark:text-white dark:hover:bg-neutral-800 bg-white hover:bg-slate-100 py-2.5 px-4 border-[1px] rounded-lg font-medium text-sm lg:text-base disabled:opacity-50'
+        primary: 'dark:bg-white dark:hover:bg-slate-200 dark:text-black bg-neutral-900 hover:bg-neutral-900/90 text-white py-3 px-6 rounded-md font-medium text-sm lg:text-base disabled:opacity-50',
+        secondary: 'dark:bg-neutral-950 dark:border-neutral-800 dark:text-white dark:hover:bg-neutral-800 bg-white hover:bg-slate-100 py-3 px-6 border-[1px] rounded-md font-medium text-sm lg:text-base disabled:opacity-50'
     }
 
     return (
         <button 
             onClick={onClick}
             disabled={disabled}
-            className={`${variants[variant]}`}>
+            className={`${variants[variant]} ${className}`}>
             {
                 loading ? (
                     <div className="flex justify-center relative w-5 h-5">
