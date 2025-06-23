4dbe1bb99619c4b954ca249fcf66d95d154655f4
Doniyor Aloxanov <doniyoraloxanov@gmail.com>
2025-06-23 16:37:43 +0500

    Refactor EditProjectModal and ProjectLayout components to improve project update handling and API integration.

--- src/layouts/ProjectLayout.jsx
+++ src/layouts/ProjectLayout.jsx
@@ -11,33 +11,34 @@ import { Plus, Settings, Mail, SquarePen, UserMinus, Trash2 } from "lucide-react
 function ProjectLayout() {
 
     const location = useLocation();
-    const { state: { projectInfo } } = location;
+    const { state: { projectInfo: initialProjectInfo } } = location;
 
+    const [projectInfo, setProjectInfo] = useState(initialProjectInfo);
     const [settingsButtonClicked, setSettingsButtonClicked] = useState(false);
     const [showNewTaskModal, setShowNewTaskModal] = useState(false);
     const [showEditProjectModal, setShowEditProjectModal] = useState(false);
-	const [showDeleteProjectModal, setShowDeleteProjectModal] = useState(false);
-	const [showLeaveProjectModal, setShowLeaveProjectModal] = useState(false);
-	const [showGroupEmailModal, setShowGroupEmailModal] = useState(false);
+    const [showDeleteProjectModal, setShowDeleteProjectModal] = useState(false);
+    const [showLeaveProjectModal, setShowLeaveProjectModal] = useState(false);
+    const [showGroupEmailModal, setShowGroupEmailModal] = useState(false);
 
     const teamMembers = [
         {
             id: 0,
             fullName: "John Doe",
             position: "QA Engineer",
-			email: "johndoe@gmail.com"
+            email: "johndoe@gmail.com"
         },
         {
             id: 1,
             fullName: "John Doe 2",
             position: "Backend Developer",
-			email: "johndoe@gmail.com"
+            email: "johndoe@gmail.com"
         },
         {
             id: 2,
             fullName: "John Doe 3",
             position: "Frontend Developer",
-			email: "johndoe@gmail.com"
+            email: "johndoe@gmail.com"
         }
     ];
 
@@ -77,9 +78,9 @@ function ProjectLayout() {
         <div className="flex flex-col h-full gap-y-6 px-6 pt-6 md:px-8 md:pt-8">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-y-6 gap-x-6 md:gap-x-8">
                 <div className="flex items-center justify-between md:justify-start gap-x-6">
-                    <h1 className="text-xl md:text-2xl lg:text-[28px] font-bold">{ projectInfo.title }</h1>
+                    <h1 className="text-xl md:text-2xl lg:text-[28px] font-bold">{projectInfo.title}</h1>
                     <div className={`${statusColors[projectInfo.status]} status-badge px-3 py-1 text-xs md:text-sm`}>
-                        { projectInfo.status }
+                        {projectInfo.status}
                     </div>
                 </div>
                 <div className="md:justify-end flex items-center gap-x-5">
@@ -90,11 +91,11 @@ function ProjectLayout() {
                         >
                             <Settings className="w-5 h-5" />
                         </button>
-                        <div className={`dark:bg-black dark:border-neutral-800 bg-white shadow-md border-neutral-200 border-[1px] 
+                        <div className={`dark:bg-black dark:border-neutral-800 bg-white shadow-md border-neutrals-200 border-[1px] 
 						rounded-md flex flex-col absolute w-[200px] mt-3 left-0 ${settingsButtonClicked ? 'opacity-100' :
-						'opacity-0 pointer-events-none'} transition-[opacity] duration-200 z-10`}>
+                                'opacity-0 pointer-events-none'} transition-[opacity] duration-200 z-10`}>
                             <div className="flex flex-col border-b-[1px] dark:border-neutral-800 border-neutral-200 p-1.5">
-                                <button 
+                                <button
                                     className="dark:hover:bg-neutral-900 hover:bg-slate-100 rounded-md flex 
                                     items-center gap-x-4 px-2.5 py-1.5 transition[background-color] duration-200"
                                     onClick={() => {
@@ -105,38 +106,38 @@ function ProjectLayout() {
                                     <SquarePen className="w-4 h-4" />
                                     <span>Edit Project</span>
                                 </button>
-                                <button 
-									className="dark:hover:bg-neutral-900 hover:bg-slate-100 rounded-md flex items-center 
+                                <button
+                                    className="dark:hover:bg-neutral-900 hover:bg-slate-100 rounded-md flex items-center 
                                 	gap-x-4 px-2.5 py-1.5 transition[background-color] duration-200"
-									onClick={() => {
-										setSettingsButtonClicked(false);
-										setShowGroupEmailModal(true);
-									}}
-								>
+                                    onClick={() => {
+                                        setSettingsButtonClicked(false);
+                                        setShowGroupEmailModal(true);
+                                    }}
+                                >
                                     <Mail className="w-4 h-4" />
                                     <span>Send Email</span>
                                 </button>
                             </div>
                             <div className="flex flex-col p-1.5">
-                                <button 
-									className="dark:hover:bg-neutral-900 hover:bg-slate-100 rounded-md flex items-center 
+                                <button
+                                    className="dark:hover:bg-neutral-900 hover:bg-slate-100 rounded-md flex items-center 
                                 	gap-x-4 px-2.5 py-1.5 transition[background-color] duration-200"
-									onClick={() => {
-										setSettingsButtonClicked(false);
-										setShowLeaveProjectModal(true);
-									}}
-								>
+                                    onClick={() => {
+                                        setSettingsButtonClicked(false);
+                                        setShowLeaveProjectModal(true);
+                                    }}
+                                >
                                     <UserMinus className="w-4 h-4 text-orange-500" />
                                     <span className="text-orange-500">Leave Project</span>
                                 </button>
-                                <button 
-									className="dark:hover:bg-neutral-900 hover:bg-slate-100 rounded-md flex items-center 
+                                <button
+                                    className="dark:hover:bg-neutral-900 hover:bg-slate-100 rounded-md flex items-center 
                                 	gap-x-4 px-2.5 py-1.5 transition[background-color] duration-200"
-									onClick={() => {
-										setSettingsButtonClicked(false);
-										setShowDeleteProjectModal(true);
-									}}
-								>
+                                    onClick={() => {
+                                        setSettingsButtonClicked(false);
+                                        setShowDeleteProjectModal(true);
+                                    }}
+                                >
                                     <Trash2 className="w-4 h-4 text-red-500" />
                                     <span className="text-red-500">Delete Project</span>
                                 </button>
@@ -211,50 +212,51 @@ function ProjectLayout() {
                 </NavLink>
             </ul>
             <Outlet context={{ projectData: projectInfo }} />
-			{
+            {
                 showNewTaskModal && (
-                    <NewTaskModal 
-                        setShowNewTaskModal={setShowNewTaskModal} 
-                        teamMembers={teamMembers} 
-                    />  
+                    <NewTaskModal
+                        setShowNewTaskModal={setShowNewTaskModal}
+                        teamMembers={teamMembers}
+                    />
                 )
             }
             {
                 showEditProjectModal && (
-                    <EditProjectModal 
+                    <EditProjectModal
                         projectId={projectInfo.id}
-                        currentProjectTitle={projectInfo.title} 
-                        currentProjectStatus={projectInfo.status}  
+                        currentProjectTitle={projectInfo.title}
+                        currentProjectStatus={projectInfo.status}
                         showModal={setShowEditProjectModal}
+                        onProjectUpdated={(updatedProject) => setProjectInfo((prev) => ({ ...prev, ...updatedProject }))}
                     />
                 )
             }
-			{
-				showDeleteProjectModal && (
-					<DeleteProjectModal
-					 	projectId={projectInfo.id}
-						projectTitle={projectInfo.title}
-						showModal={setShowDeleteProjectModal}
-					/>
-				)
-			}
-			{
-				showLeaveProjectModal && (
-					<LeaveProjectModal 
-						projectId={projectInfo.id}
-						projectTitle={projectInfo.title}
-						showModal={setShowLeaveProjectModal}
-					/>
-				)
-			}
-			{
-				showGroupEmailModal && (
-					<GroupEmailModal 
-						teamMembers={teamMembers}
-						showModal={setShowGroupEmailModal}
-					/>
-				)
-			}
+            {
+                showDeleteProjectModal && (
+                    <DeleteProjectModal
+                        projectId={projectInfo.id}
+                        projectTitle={projectInfo.title}
+                        showModal={setShowDeleteProjectModal}
+                    />
+                )
+            }
+            {
+                showLeaveProjectModal && (
+                    <LeaveProjectModal
+                        projectId={projectInfo.id}
+                        projectTitle={projectInfo.title}
+                        showModal={setShowLeaveProjectModal}
+                    />
+                )
+            }
+            {
+                showGroupEmailModal && (
+                    <GroupEmailModal
+                        teamMembers={teamMembers}
+                        showModal={setShowGroupEmailModal}
+                    />
+                )
+            }
         </div>
     );
 
