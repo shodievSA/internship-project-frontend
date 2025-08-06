032606c5763a62c5d8dec11d0615cb66a1abb954
shodievSA <shodiev.s.a@gmail.com>
2025-08-06 15:30:13 +0500

    add current date in top bar

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -7,11 +7,9 @@ function Topbar({ sidebarCollapsed, setSidebarCollapsed }) {
 	const { themeMode, setThemeMode } = useThemeContext();
 
 	return (
-		<div
-			className="dark:bg-black bg-white dark:text-white dark:border-neutral-800 flex items-center 
+		<div className="dark:bg-black bg-white dark:text-white dark:border-neutral-800 flex items-center 
         justify-between p-5 h-14 flex-shrink-0 border-b-[1px] border-neutral-200 lg:rounded-ss-xl 
-		lg:rounded-tr-xl"
-		>
+		lg:rounded-tr-xl">
 			<div className="flex items-center gap-x-1">
 				<button
 					className="dark:hover:bg-neutral-900 hover:bg-neutral-100 p-2 rounded-md"
@@ -20,18 +18,20 @@ function Topbar({ sidebarCollapsed, setSidebarCollapsed }) {
 					<PanelRight className="w-4 h-4 rotate-180" />
 				</button>
 				<div>
-					<h3 className="font-medium text-sm lg:text-base">
+					<h3 className="text-sm lg:text-base">
 						{displayGreetingMessage(user.fullName.split(" ")[0])}
 					</h3>
 				</div>
 			</div>
-			<button
-				className="dark:hover:bg-neutral-900 dark:border-neutral-800 p-2 border-[1px] rounded-md 
+			<div>
+				<span className="font-medium text-sm lg:text-base">
+					{ displayCurrentDate() }
+				</span>
+			</div>
+			<button 
+			className="dark:hover:bg-neutral-900 dark:border-neutral-800 p-2 border-[1px] rounded-md 
             border-neutral-200 hover:bg-neutral-50"
-				onClick={() =>
-					setThemeMode(themeMode === "light" ? "dark" : "light")
-				}
-			>
+			onClick={() => setThemeMode(themeMode === "light" ? "dark" : "light")}>
 				{themeMode === "light" ? (
 					<Sun className="w-4 h-4" />
 				) : (
@@ -43,6 +43,7 @@ function Topbar({ sidebarCollapsed, setSidebarCollapsed }) {
 }
 
 function displayGreetingMessage(userName) {
+
 	const date = new Date();
 	const hours = date.getHours();
 
@@ -55,6 +56,21 @@ function displayGreetingMessage(userName) {
 	} else {
 		return "Good night, " + userName;
 	}
+
 }
+
+function displayCurrentDate() {
+
+	const now = new Date();
+
+	const day = now.getDate();
+	const fullDay = now.toLocaleDateString('en-US', { weekday: 'long' });
+
+	const formatted = `${fullDay}, ${day}`;
+
+	return formatted;
+
+}
+
 
 export default Topbar;
