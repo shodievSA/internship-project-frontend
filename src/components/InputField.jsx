6115ccde89b34c3a9861bfc72f2b78954493201e
Asad <asadrahmonberdiev@gmail.com>
2025-09-04 20:34:19 +0500

    Refactor sprint and task modals for improved functionality and UI consistency
    - Updated EditSprintModal to remove unused TextareaField and streamline state management.
    - Enhanced InputField component to include character limit functionality.
    - Modified NewProjectModal to enforce character limits on input fields.
    - Simplified NewSprintModal by removing unnecessary description field and ensuring consistent input handling.
    - Improved UpdateTaskModal by refining state management and ensuring proper file handling during updates.

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -3,55 +3,57 @@ import Input from "./ui/Input";
 import { Asterisk, CircleAlert } from "lucide-react";
 
 function InputField({
-	label,
-	disabled,
-	placeholder,
-	required = false,
-	value,
-	setValue,
-	error,
+    label,
+    disabled,
+    placeholder,
+    required = false,
+    value,
+    charLimit,
+    setValue,
+    error,
 }) {
-	const [showError, setShowError] = useState(false);
+    const [showError, setShowError] = useState(false);
 
-	return (
-		<div className="flex flex-col gap-y-3 w-full">
-			<label className="flex gap-x-0.5">
-				<span className="dark:text-white text-sm md:text-base font-semibold">
-					{label}
-				</span>
-				{required && (
-					<Asterisk className="w-3 h-3 mt-0.5 text-red-500" />
-				)}
-			</label>
-			{required ? (
-				<>
-					<Input
-						placeholder={placeholder}
-						value={value}
-						onChange={(e) => {
-							const value = e.target.value;
-							setShowError(!value);
-							setValue(value);
-						}}
-						disabled={disabled}
-					/>
-					{showError && (
-						<div className="flex gap-x-1.5 text-red-500">
-							<CircleAlert className="w-4 h-4" />
-							<p className="text-sm">{error}</p>
-						</div>
-					)}
-				</>
-			) : (
-				<Input
-					placeholder={placeholder}
-					value={value}
-					onChange={(e) => setValue(e.target.value)}
-					disabled={disabled}
-				/>
-			)}
-		</div>
-	);
+    return (
+        <div className="flex flex-col gap-y-3 w-full">
+            <label className="flex gap-x-0.5">
+                <span className="dark:text-white text-sm md:text-base font-semibold">
+                    {label}
+                </span>
+                {required && (
+                    <Asterisk className="w-3 h-3 mt-0.5 text-red-500" />
+                )}
+            </label>
+            {required ? (
+                <>
+                    <Input
+                        placeholder={placeholder}
+                        value={value}
+                        onChange={(e) => {
+                            const value = e.target.value;
+                            setShowError(!value);
+                            setValue(value);
+                        }}
+                        charLimit={charLimit}
+                        disabled={disabled}
+                    />
+                    {showError && (
+                        <div className="flex gap-x-1.5 text-red-500">
+                            <CircleAlert className="w-4 h-4" />
+                            <p className="text-sm">{error}</p>
+                        </div>
+                    )}
+                </>
+            ) : (
+                <Input
+                    placeholder={placeholder}
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                    disabled={disabled}
+                />
+            )}
+        </div>
+    );
 }
 
 export default InputField;
