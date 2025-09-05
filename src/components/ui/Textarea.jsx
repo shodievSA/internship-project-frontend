98c95addc871874611c6daf979f653663a44110f
shodievSA <shodiev.s.a@gmail.com>
2025-09-05 19:26:00 +0500

    create separate component for a modal window which allows users to submit tasks (SubmitTaskModal.jsx); remove unused imports; modify CSS styles for TextareaField.jsx and Textarea.jsx components

--- src/components/ui/Textarea.jsx
+++ src/components/ui/Textarea.jsx
@@ -1,4 +1,11 @@
-function Textarea({ disabled, value, placeholder, rows, onChange }) {
+function Textarea({ 
+	disabled, 
+	value, 
+	placeholder, 
+	rows, 
+	onChange 
+}) {
+
 	return (
 		<textarea
 			disabled={disabled}
@@ -6,12 +13,13 @@ function Textarea({ disabled, value, placeholder, rows, onChange }) {
 			placeholder={placeholder}
 			rows={rows}
 			className={`dark:bg-neutral-950 dark:border-neutral-800 dark:focus:border-neutral-600 
-			focus:border-black/30 bg-white resize-none rounded-md text-sm border-[1px] w-full 
+			focus:border-black/30 bg-white resize-none rounded-md  border-[1px] w-full 
 			py-2.5 px-4 outline-none disabled:opacity-50 disabled:cursor-default cursor-text
 			scrollbar-thin dark:scrollbar-thumb-neutral-950 dark:scrollbar-track-neutral-800`}
 			onChange={onChange}
 		/>
 	);
+
 }
 
 export default Textarea;
